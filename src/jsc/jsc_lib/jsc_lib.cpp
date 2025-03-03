b05c9a356cded8e76726e6573f803b19bfeed51c
Jain, Vinod Kumar <VinodKumar_Jain@comcast.com>
2025-03-03 12:22:07 -0600

    RDK-56154 : Merging all rdknativescript latest changes to rdkcentral github from comcast rdke github
    Reason for change: Simplying and arranging the licensing for code.
    Test Procedure: build should be successful.
    Risks: low
    Priority: P1

--- src/jsc/jsc_lib/jsc_lib.cpp
+++ src/jsc/jsc_lib/jsc_lib.cpp
@@ -1,27 +1,4 @@
-/**
-* If not stated otherwise in this file or this component's LICENSE
-* file the following copyright and licenses apply:
-*
-* Copyright 2024 RDK Management
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
-
 /*
- * Copyright (C) 1999-2000 Harri Porten (porten@kde.org)
- * Copyright (C) 2004-2022 Apple Inc. All rights reserved.
- * Copyright (C) 2006 Bjoern Graf (bjoern.graf@gmail.com)
- *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation;
