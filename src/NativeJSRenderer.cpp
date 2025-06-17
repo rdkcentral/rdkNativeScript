7ec702a0b9847846ce375063c57fd04aed883dd2
Gurpreet Sarngal <gurpreetsarngal@gmail.com>
2025-06-17 12:09:14 +0530

    RDKEMW-5230: JSRuntimeLauncher component - RDKE
    Reason for change: Removed JSRuntimeLauncher
    Test Procedure: build should be successful.
    Risks: low
    Priority: P2

--- src/NativeJSRenderer.cpp
+++ src/NativeJSRenderer.cpp
@@ -326,29 +326,31 @@ void NativeJSRenderer::runApplicationInternal(ApplicationRequest& appRequest)
 			    return ;
 			}
 			JavaScriptContext* context = (JavaScriptContext*)mContextMap[id].context;
-		        #if defined(ENABLE_JSRUNTIME_LAUNCHER)
-			std::stringstream window;
-            		window<<"window.location = {\"href\":\"" << url << "\"};";
-           		NativeJSLogger::log(INFO, "Adding the window location: %s to js file\n", window.str().c_str());
-            		context->runScript(window.str().c_str(),true, url, nullptr, true);
-			#endif
+			if(context->getModuleSettings().enableJSDOM)
+			{
+				std::stringstream window;
+            	    		window<<"window.location = {\"href\":\"" << url << "\"};";
+           			NativeJSLogger::log(INFO, "Adding the window location: %s to js file\n", window.str().c_str());
+            			context->runScript(window.str().c_str(),true, url, nullptr, true);
+			}
 			NativeJSLogger::log(INFO, "nativeJS application thunder execution url: %s, result: %d\n", url.c_str(), ret ? 1 : 0);
 			ret = context->runScript(chunk.contentsBuffer, true, url, nullptr, true);
 			NativeJSLogger::log(INFO, "nativeJS application execution result: %d\n", ret ? 1 : 0);
 			double duration = context->getExecutionDuration();
-                        context->setAppdata(id, url);
+            		context->setAppdata(id, url);
 			NativeJSLogger::log(INFO, "Execution duration(runApplicationDuration) for ID %d | URL %s : %.3f ms\n", id, url.c_str(), duration);
 		}
 		else
 		{	    
 			NativeJSLogger::log(INFO, "About to launch local app\n");
 			JavaScriptContext* context = (JavaScriptContext*)mContextMap[id].context;
-            		#if defined(ENABLE_JSRUNTIME_LAUNCHER)
-			std::stringstream window;
-            		window<<"window.location = {\"href\":\"file:/" << url << "\"};";
-            		NativeJSLogger::log(INFO, "Adding the window location: %s to js file\n", window.str().c_str());
-            		context->runScript(window.str().c_str(),true, url, nullptr, true);
-			#endif
+            		if(context->getModuleSettings().enableJSDOM)
+            		{
+			    std::stringstream window;
+                	    window<<"window.location = {\"href\":\"file:/" << url << "\"};";
+                	    NativeJSLogger::log(INFO, "Adding the window location: %s to js file\n", window.str().c_str());
+                	    context->runScript(window.str().c_str(),true, url, nullptr, true);
+			}
 			NativeJSLogger::log(INFO, "Running test application: %s\n", url.c_str());
 			bool ret = context->runFile(url.c_str(), nullptr, true);
 			NativeJSLogger::log(INFO, "Test application execution result: %d\n", ret ? 1 : 0);
